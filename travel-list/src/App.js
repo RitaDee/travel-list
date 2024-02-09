d2a7f485e54fc57aca7d9c7d66a187df27f38b59
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-02-09 11:34:15 +0100

    feat: add items to packing list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { Form } from "./Form";
 import { Logo } from "./Logo";
 import { PackingList } from "./PackingList";
 import { Stat } from "./Stat";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stat />
     </div>
   );
--- travel-list/src/Form.js
+++ travel-list/src/Form.js
@@ -1,18 +1,45 @@
-export const Form = () => {
+import { useState } from "react";
+
+export const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+    console.log(newItem);
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
   };
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." />
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
