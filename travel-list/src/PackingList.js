42b1514668d06f50629f736a194e54ffb332df07
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-02-13 14:06:28 +0100

    refactor: remove unused code

--- travel-list/src/PackingList.js
+++ travel-list/src/PackingList.js
@@ -1,9 +1,4 @@
 import { Item } from "./Item";
-const initialItems = [
-	{ id: 1, description: "Passports", quantity: 2, packed: false },
-	{ id: 2, description: "Socks", quantity: 12, packed: true },
-	{ id: 3, description: "Charger", quantity: 1, packed: false },
-];
 
 export const PackingList = ({ items, onDeleteItem }) => {
 	return (
