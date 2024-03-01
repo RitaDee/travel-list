d627498bd612811fbdd482262e000e5a9ded6f29
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-03-01 20:26:55 +0100

    feat: Add sorting functionality to PackingList component

--- travel-list/src/PackingList.js
+++ travel-list/src/PackingList.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { Item } from "./Item";
 
 export const PackingList = ({ items, onDeleteItem, onToggle }) => {
-	return (
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+
+  if(sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+	
+  return (
 		<div className="list">
 			<ul>
-				{items.map((item) => (
+				{sortedItems.map((item) => (
 					<Item
 						item={item}
 						onDeleteItem={onDeleteItem}
@@ -12,7 +22,15 @@ export const PackingList = ({ items, onDeleteItem, onToggle }) => {
 						key={item.id}
 					/>
 				))}
-			</ul>
+      </ul>
+      
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
 		</div>
 	);
 };
