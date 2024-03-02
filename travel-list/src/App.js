d8f09f2f20a2bc0c764fb7a34765f72e2f2d0d0c
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-03-02 22:11:16 +0100

    feat: Add confirmation dialog before clearing the list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -24,7 +24,8 @@ const App = () => {
   };
   
   const handleClearList = () => { 
-    setItems([]);
+    const confirmed = window.confirm('Are you sure you want to delete all items?');
+    if (confirmed) setItems([]);
   
   }
 
